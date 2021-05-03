dcd9e068b3129fd55cd8257933c11006fc835ba0
black996 <bl4ck.96@yandex.com>
2021-05-03 23:13:26 +0300

    backend for rock-events-nextjs app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dfb3f7c95f67876a76cddd654313e837'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '193fe913-51ce-4448-b8fd-07c672fb6b57'
+};
